3a61878a2ef6db1123b9ebf3b4c98e2a91842e9c
joeybodoia <jbodoia21@cmc.edu>
2024-10-14 19:18:07 -0700

    added gazebos

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Spa from './pages/Spa';
 import Swim from './pages/Swim';
+import Gazebo from './pages/Gazebo';  // Import the new Gazebo page
 import './App.css'; // Import your CSS file
 
 const App = () => {
   return (
     <Router>
-      {/* New Top Nav Bar */}
+      {/* Top Nav Bar */}
       <nav className="top-navbar">
         <div className="top-navbar-left">
           Call to schedule a private appointment to view our showroom @ 253-696-9696
@@ -19,12 +20,13 @@ const App = () => {
         </div>
       </nav>
 
-      {/* Existing Nav Bar */}
+      {/* Main Nav Bar */}
       <nav className="navbar">
         <ul className="nav-list">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/spa">Spa</Link></li>
           <li><Link to="/swim">Swim</Link></li>
+          <li><Link to="/gazebo">Gazebo</Link></li> {/* Add Gazebo to the nav */}
         </ul>
       </nav>
 
@@ -32,6 +34,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/spa" element={<Spa />} />
         <Route path="/swim" element={<Swim />} />
+        <Route path="/gazebo" element={<Gazebo />} /> {/* Add Gazebo route */}
       </Routes>
     </Router>
   );  
---
+++ src/pages/Gazebo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Gazebo = () => {
+  return (
+    <div className="gazebo-container">
+      <h1>Gazebo Page</h1>
+      <p>
+        Explore our collection of elegant gazebos designed to transform your outdoor space into a luxurious retreat. 
+        Whether you're looking for a cozy hideaway or a space for entertaining guests, our gazebos are perfect for any occasion.
+      </p>
+    </div>
+  );
+};
+
+export default Gazebo;
