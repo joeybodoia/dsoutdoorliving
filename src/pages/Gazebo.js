8af1f89833e9e8e9a8e18052f90f340063997056
joeybodoia <jbodoia21@cmc.edu>
2024-12-05 10:24:52 -0800

    styling changes

--- src/pages/Gazebo.js
+++ src/pages/Gazebo.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Ensure Font Awesome is imported
 
 const Gazebo = () => {
   const [gazeboProducts, setGazeboProducts] = useState([]);
@@ -10,15 +11,15 @@ const Gazebo = () => {
     axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/gazebos`)
       .then((response) => {
         setGazeboProducts(response.data);
-      })
+      })  
       .catch((error) => {
         console.error('Error fetching gazebo products:', error);
-      });
+      }); 
   }, []);
 
   const handleProductClick = (productName) => {
     navigate(`/gazebos/${productName}`);
-  };
+  };  
 
   return (
     <div className="spa-container">
@@ -29,20 +30,22 @@ const Gazebo = () => {
             key={index} 
             className="spa-item" 
             onClick={() => handleProductClick(gazebo.PRODUCT_NAME)}
-          >
+          >   
             <img 
               src={Object.values(gazebo.IMAGE_URLS)[0]} 
               alt={`Gazebo ${gazebo.PRODUCT_NAME}`} 
               className="spa-image"
-            />
+            />  
             <h2>{gazebo.PRODUCT_NAME}</h2>
-            <p>Number of People: {gazebo.PRODUCT_DESCRIPTION["Number of People"]}</p>
-            <p>Measurements: {gazebo.PRODUCT_SPECIFICATIONS[0]["General Specifications"]["Measurements (inch)"]}</p>
+            <p className="right-text">
+              <i className="fas fa-box"></i> {/* Icon for Measurements */}
+              {gazebo.PRODUCT_SPECIFICATIONS[0]["General Specifications"]["Measurements (inch)"]}
+            </p>
           </div>
-        ))}
+        ))} 
       </div>
     </div>
-  );
+  );  
 };
 
 export default Gazebo;
--- src/pages/Swim.js
+++ src/pages/Swim.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Ensure Font Awesome is imported
 
 const Swim = () => {
   const [swimProducts, setSwimProducts] = useState([]);
@@ -10,15 +11,15 @@ const Swim = () => {
     axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/swimspas`)
       .then((response) => {
         setSwimProducts(response.data);
-      })
+      })  
       .catch((error) => {
         console.error('Error fetching swim spa products:', error);
-      });
+      }); 
   }, []);
 
   const handleProductClick = (productName) => {
     navigate(`/swimspas/${productName}`);
-  };
+  };  
 
   return (
     <div className="spa-container">
@@ -29,20 +30,26 @@ const Swim = () => {
             key={index} 
             className="spa-item" 
             onClick={() => handleProductClick(swim.PRODUCT_NAME)}
-          >
+          >   
             <img 
               src={Object.values(swim.IMAGE_URLS)[0]} 
               alt={`Swim Spa ${swim.PRODUCT_NAME}`} 
               className="spa-image"
-            />
+            />  
             <h2>{swim.PRODUCT_NAME}</h2>
-            <p>Capacity: {swim.PRODUCT_DESCRIPTION["Number of People"]}</p>
-            <p>Measurements: {swim.PRODUCT_SPECIFICATIONS[0]["General Specifications"]["Measurements (inch)"]}</p>
+            <p>
+              <i className="fas fa-users"></i> {/* Icon for capacity */}
+              {swim.PRODUCT_DESCRIPTION["Number of People"]}
+            </p>
+            <p className="right-text">
+              <i className="fas fa-box"></i> {/* Icon for dimensions */}
+              {swim.PRODUCT_SPECIFICATIONS[0]["General Specifications"]["Measurements (inch)"]}
+            </p>
           </div>
-        ))}
+        ))} 
       </div>
     </div>
-  );
+  );  
 };
 
 export default Swim;
