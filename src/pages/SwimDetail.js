6f063a8b85e66e7e7ea19bc2adf04e4acf51c70a
joeybodoia <jbodoia21@cmc.edu>
2025-01-27 15:42:38 -0800

    SwimDetail Updated

--- src/pages/SwimDetail.js
+++ src/pages/SwimDetail.js
@@ -11,24 +11,31 @@ const SwimDetail = () => {
   const [accessoriesVisible, setAccessoriesVisible] = useState({}); // State for accessories visibility
 
   useEffect(() => {
-    // Fetch the specific swim spa data using the product name
-    axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/swimspas/${productName}`)
+    // Fetch the specific spa product data using the product name
+    axios
+      .get(`${process.env.REACT_APP_API_BASE_URL}/api/spas/${productName}`)
       .then((response) => {
         setSwim(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching swim spa details:', error);
+        console.error("Error fetching spa details:", error);
       });
   }, [productName]);
 
   if (!swim) return <div>Loading...</div>; // Loading state
 
   const handleNextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % Object.keys(swim.IMAGE_URLS).length);
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex + 1) % Object.keys(swim.IMAGE_URLS).length
+    );
   };
 
   const handlePrevImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + Object.keys(swim.IMAGE_URLS).length) % Object.keys(swim.IMAGE_URLS).length);
+    setCurrentImageIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + Object.keys(swim.IMAGE_URLS).length) %
+        Object.keys(swim.IMAGE_URLS).length
+    );
   };
 
   const toggleSpecification = (specName) => {
@@ -38,49 +45,96 @@ const SwimDetail = () => {
     }));
   };
 
-  const toggleAccessory = (accessoryTitle) => {
-    setAccessoriesVisible((prev) => ({
-      ...prev,
-      [accessoryTitle]: !prev[accessoryTitle], // Toggle visibility
-    }));
-  };
-
-  // Define the specification titles
   const specificationTitles = [
     "General Specifications",
     "Jets Specifications",
     "Standard Pumps Configuration",
     "Control System Selection",
-    "Filtration and Purification"
+    "Filtration and Purification",
   ];
 
   return (
     <div className="spa-detail">
-      <div className="spa-description-container">
-        <div className="image-container">
-          <div className="image-carousel-container">
-            <div className="image-carousel">
-              <button onClick={handlePrevImage}>&lt;</button>
-              <img 
-                src={Object.values(swim.IMAGE_URLS)[currentImageIndex]} 
-                alt={`Swim Spa ${swim.PRODUCT_NAME}`} 
-                className="carousel-image"
-              />
-              <button onClick={handleNextImage}>&gt;</button>
+      <div className="teal-container">
+        <div className="image-description-container">
+          {/* Image Carousel */}
+          <div className="image-container">
+            <div className="image-carousel-container">
+              <div className="image-carousel">
+                <img
+                  src={Object.values(swim.IMAGE_URLS)[currentImageIndex]}
+                  alt={`${swim.PRODUCT_NAME}`}
+                  className="carousel-image"
+                />
+              </div>
+            </div>
+            <div className="thumbnail-carousel">
+              <button
+                onClick={handlePrevImage}
+                className="carousel-nav left-nav"
+              >
+                &lt;
+              </button>
+              <div className="thumbnails">
+                {Object.values(swim.IMAGE_URLS).map((imageUrl, index) => (
+                  <img
+                    key={index}
+                    src={imageUrl}
+                    alt={`Thumbnail ${index + 1}`}
+                    className={`thumbnail ${
+                      currentImageIndex === index
+                        ? "active-thumbnail"
+                        : ""
+                    }`}
+                    onClick={() => setCurrentImageIndex(index)}
+                  />
+                ))}
+              </div>
+              <button
+                onClick={handleNextImage}
+                className="carousel-nav right-nav"
+              >
+                &gt;
+              </button>
             </div>
           </div>
-        </div>
-        <div className="description-container">
-          <h1>{swim.PRODUCT_NAME}</h1>
-          <h3>Description</h3>
-          <p>{swim.PRODUCT_DESCRIPTION.Description}</p>
-          <p>Seats: {swim.PRODUCT_DESCRIPTION['Number of Seats']}</p>
-          <p>Gallons: {swim.PRODUCT_DESCRIPTION.Gallons}</p>
-          <p>Weight: {swim.PRODUCT_DESCRIPTION.Weight} lbs</p>
+
+          {/* Description */}
+          <div className="description-container">
+            <h1>{swim.PRODUCT_NAME}</h1>
+            <hr className="title-divider" />
+            <p className="description-text">
+              {swim.PRODUCT_DESCRIPTION.Description}
+            </p>
+            <ul className="description-icons">
+              <li>
+                <i className="fas fa-users"></i>{" "}
+                {swim.PRODUCT_DESCRIPTION["Number of People"]} People
+              </li>
+              <li>
+                <i className="fas fa-chair"></i>{" "}
+                {swim.PRODUCT_DESCRIPTION["Number of Seats"]} Seats
+              </li>
+              <li>
+                <i className="fas fa-weight-hanging"></i>{" "}
+                {swim.PRODUCT_DESCRIPTION.Weight} lbs
+              </li>
+              <li>
+                <i className="fas fa-ruler-combined"></i>{" "}
+                {`${swim.PRODUCT_DESCRIPTION.Length} x ${swim.PRODUCT_DESCRIPTION.Width} x ${swim.PRODUCT_DESCRIPTION.Height}`}{" "}
+                inches
+              </li>
+              <li>
+                <i className="fas fa-water"></i>{" "}
+                {swim.PRODUCT_DESCRIPTION.Gallons} Gallons
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
 
-      <h2 style={{ textAlign: 'center' }}>Product Features</h2>
+      {/* Product Features */}
+      <h2 style={{ textAlign: "center" }}>Product Features</h2>
       <div className="features-container">
         {swim.PRODUCT_FEATURES.map((feature, index) => (
           <div key={index} className="feature-item">
@@ -91,16 +145,22 @@ const SwimDetail = () => {
         ))}
       </div>
 
-      <h2 style={{ textAlign: 'center' }}>Product Specifications</h2>
+      {/* Product Specifications */}
+      <h2 style={{ textAlign: "center" }}>Product Specifications</h2>
       <div className="specifications-container">
         {specificationTitles.map((title, index) => (
           <div key={index}>
-            <button onClick={() => toggleSpecification(title)} className="specification-button">
-              {title} {specificationsVisible[title] ? '-' : '+'}
+            <button
+              onClick={() => toggleSpecification(title)}
+              className="specification-button"
+            >
+              {title} {specificationsVisible[title] ? "-" : "+"}
             </button>
             {specificationsVisible[title] && (
               <div className="specification-details">
-                {Object.entries(swim.PRODUCT_SPECIFICATIONS[index][title]).map(([key, value], idx) => (
+                {Object.entries(
+                  swim.PRODUCT_SPECIFICATIONS[index][title]
+                ).map(([key, value], idx) => (
                   <p key={idx}>{`${key}: ${value}`}</p>
                 ))}
               </div>
@@ -109,13 +169,18 @@ const SwimDetail = () => {
         ))}
       </div>
 
-      <h2 style={{ textAlign: 'center' }}>Accessories</h2> {/* Centered header for accessories */}
+      {/* Accessories */}
+      <h2 style={{ textAlign: "center" }}>Accessories</h2>
       <div className="accessories-container">
         {swim.PRODUCT_ACCESSORIES.map((accessory, index) => (
           <div key={index} className="accessory-item">
             <div className="accessory-content">
               <h4>{accessory.title}</h4>
-              <img src={accessory.image_url} alt={`Accessory ${accessory.title}`} className="accessory-image" />
+              <img
+                src={accessory.image_url}
+                alt={`Accessory ${accessory.title}`}
+                className="accessory-image"
+              />
               <p className="accessory-description">{accessory.description}</p>
             </div>
           </div>
@@ -126,4 +191,3 @@ const SwimDetail = () => {
 };
 
 export default SwimDetail;
-
